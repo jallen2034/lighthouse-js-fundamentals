e26ee51d5450a4c8f297aacf611397a5820f32b3
Jacob Allen <jallen2033@gmail.com>
2020-12-08 04:48:09 +0000

    even.js commit

---
+++ even.js
@@ -0,0 +1,19 @@
+/* isEven takes as its parameter a number and returns a Boolean value representing whether or not the number is even.
+const isEven = function(num){
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+*/
+
+// simplified:
+const isEven  = function(num){
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
