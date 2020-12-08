ccd55c271278a6ea51ac52c0a657d090f08e1a21
Jacob Allen <jallen2033@gmail.com>
2020-12-08 04:27:09 +0000

    hello.js commit

---
+++ hello.js
@@ -0,0 +1,17 @@
+
+/* first hello world function
+const sayHello = function(){
+  console.log("Hello, world");
+}
+
+sayHello();
+*/
+
+// second function with incoming parameter 
+const sayHello = function(name){
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
